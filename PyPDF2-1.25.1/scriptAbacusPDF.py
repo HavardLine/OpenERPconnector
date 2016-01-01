51d73964f3fa1471eb18555a69bd5b5ff9263a2c
Havard <havard.line@gmail.com>
2016-01-01 23:32:05 +0100

    Revert "Generalized reader script"
    This reverts commit 8b7115374014985ad631780d0eb17aecba556dbf.

--- PyPDF2-1.25.1/scriptAbacusPDF.py
+++ PyPDF2-1.25.1/scriptAbacusPDF.py
@@ -4,10 +4,6 @@ from reportlab.pdfgen import canvas
 import glob, os
 from os.path import expanduser
 
-class MergePDF:
-  def __init__(self, paths, outPath):
-    print 'mergie text'
-
 #Make variables for output PDF and array for input PDFs
 output = PdfFileWriter()
 
@@ -25,23 +21,25 @@ c.save()
 #Make variable with new date-pdf
 inputDato = PdfFileReader(open("DatoStempel.pdf", "rb"))
 
-
+#Make array with filepaths
+folderlist = ([r"\server\LTS\bedrift\ABACUS_arbeidsmappe\bilag",
+                r"\server\LTS\bedrift\ABACUS_arbeidsmappe\faktura_til_betaling",
+                r"\server\LTS\bedrift\ABACUS_arbeidsmappe\loenn"])
 
 #Define function for searching after PDF in folder from folderlist-array
 def searchFolder(folderlist):
-    #os.chdir(expanduser("~")+folderlist)
-    print glob.glob(expanduser("~")+folderlist+'\*.pdf')
-    #for findPDF in glob.glob("*.pdf"):
-    #    files = PdfFileReader(open(findPDF, "rb"))
-    #    print findPDF, "has %d pages." % files.getNumPages()
-    #    print 'Found file %s'  %files
-    #    print 'in folder %s:\n' %folderlist
+    os.chdir(expanduser("~")+folderlist)
+    for findPDF in glob.glob("*.pdf"):
+        files = PdfFileReader(open(findPDF, "rb"))
+        print findPDF, "has %d pages." % files.getNumPages()
+        print 'Found file %s'  %files
+        print 'in folder %s:\n' %folderlist
 
         #For-loops to add all pages from several pdf-files to one output
-     #   for page_num in range(files.numPages):
-     #       output.addPage(files.getPage(page_num))
-     #       page = files.getPage(page_num)
-     #       page.mergePage(inputDato.getPage(0))
+        for page_num in range(files.numPages):
+            output.addPage(files.getPage(page_num))
+            page = files.getPage(page_num)
+            page.mergePage(inputDato.getPage(0))
 
     return
 
@@ -52,17 +50,9 @@ def writePDF(outputPDF):
 
     return
     
+#Calling functions for search and write PDFs
+for i in range(len(folderlist)):
+    searchFolder(folderlist[i])
 
+writePDF('ABACUS PDF')
 
-
-if __name__=="__main__":
-  
-  #Make array with filepaths
-  folderlist = [r"\server\LTS\bedrift\ABACUS_arbeidsmappe\faktura_til_betaling",
-                  r"\server\LTS\bedrift\ABACUS_arbeidsmappe\bilag",
-                  r"\server\LTS\bedrift\ABACUS_arbeidsmappe\loenn"]
-
-  #Calling functions for search and write PDFs
-  searchFolder(folderlist[0])
-  #writePDF('ABACUS PDF')
-  #MergePDF(sourcePaths, outPath)
