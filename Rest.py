4e261bc93538dc35f9a183709339b7b1cfef8bce
Havard <havard.line@gmail.com>
2016-01-01 14:46:01 +0100

    Worked on getPDF

--- Rest.py
+++ Rest.py
@@ -21,31 +21,20 @@ class Connection:
   def get(self, obj, ids):
     return self._sock.execute_kw(self._db, self._uid, self._pwd, obj, 'read', ids)
 
-  def getPDF(self, ids=[0]):
-    printsock = xmlrpclib.ServerProxy(self._uri + '/xmlrpc/report')
-    model = 'account.invoice'
-    id_report = printsock.report(self._db, self._uid, self._pwd, model, ids, {'model': model, 'id': ids[0], 'report_type':'pdf'})
-    time.sleep(5)
-    state = False
-    attempt = 0
-    while not state:
-      report = printsock.report_get(self._db, self._uid, self._pwd, id_report)
-      state = report['state']
-      if not state:
-        time.sleep(1)
-        attempt += 1
-      if attempt>30:
-        print 'Printing aborted, too long delay !'
+  def getPDF(self, ids):
+    report = xmlrpclib.ServerProxy('{}/xmlrpc/2/report'.format(self._uri))
+    result = report.render_report(self._db, self._uid, self._pwd, 'account.report_invoice', ids)
+    report_data = result['result'].decode('base64')
 
-    string_pdf = base64.decodestring(report['result'])
-    file_pdf = open('/tmp/file.pdf','w')
-    file_pdf.write(string_pdf)
+
+    file_pdf = open('invoices.pdf','w')
+    file_pdf.write(report_data)
     file_pdf.close()
     return True
 
 #Test-code for module
 if __name__ == '__main__':
   con = Connection()
-  print con.get('product.pricelist', [1])
-  #print con.getPDF() 
+  ids = con.search('account.invoice')
+  print con.getPDF(ids) 
 
