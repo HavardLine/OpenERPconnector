44b2dc27f49f33e07d0204805340aac748a7c60d
Havard <havard.line@gmail.com>
2016-06-07 23:39:24 +0200

    encoding

--- task_reader.py
+++ task_reader.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import odoo
 
@@ -14,7 +13,7 @@ task_ids = []
 for data in dataset:
   task_ids.append(data['task_id'][1])
 active_tasks = set(task_ids)
-print 'active tasks:',list(active_tasks)
+print 'active tasks:',list(active_tasks.encode(encoding='UTF-8'))
 
 for task in active_tasks:
   print ''
@@ -22,6 +21,10 @@ for task in active_tasks:
   total_hours = 0
   for data in dataset:
     if data['task_id'][1]==task:
-      print data['date'][:10], '  ', str(data['hours'])+'t', '  ',data['display_name']
+      print data['date'][:10], \
+        '  ', \
+        str(data['hours'])+'t', \
+        '  ', \
+        data['display_name'].encode(encoding='UTF-8')
       total_hours += data['hours']
   print 'Total: '+str(total_hours)+'t'
