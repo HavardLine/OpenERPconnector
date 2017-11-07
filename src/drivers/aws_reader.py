b5569dbae1b0cbf139058c5f29d4cc06c09d82b0
Håvard B Line <havard.line@gmail.com>
2017-11-07 11:39:27 +0100

    first working reader

--- src/drivers/aws_reader.py
+++ src/drivers/aws_reader.py
@@ -15,52 +15,39 @@
  */
  '''
 
-from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-import logging
-import time
+import time, json
 from os import path, environ, name
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
 # Custom MQTT message callback
 def customCallback(client, userdata, message):
-    print("Received a new message: ")
-    print(message.payload)
-    print("from topic: ")
-    print(message.topic)
-    print("--------------\n\n")
-    logger.info(message.payload)
-
+    print("--start--")
+    shadow_state = json.loads(message.payload)
+    for value in shadow_state['state']['reported']:
+        for element in shadow_state['state']['reported'][value]:
+            print(element['timedate'][:-4], element['msg'])
+    print("--end--")
+    
 # Get path to client-files
 if name == 'nt':
     path_iot = path.join(environ['localappdata'], "LTS AS", "iot")
 else:
     path_iot = environ['HOMEPATH']+"/config/iot/"
 
-# Configure logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-streamHandler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s:%(message)s")
-streamHandler.setFormatter(formatter)
-logger.addHandler(streamHandler)
+with open(path.join(path_iot, 'conf.json'), "r") as f:
+    config = json.loads(f.read())
 
-host = 'a3hdhr3r3b1d8r.iot.eu-central-1.amazonaws.com'
+host = config['host']
 rootCAPath = path.join(path_iot, 'root-CA.crt')
 certificatePath = path.join(path_iot, 'certificate.pem.crt')
 privateKeyPath = path.join(path_iot, 'private.pem.key')
-useWebsocket = False
-clientId = "no-lts-ws1"
-topic = "LOGGING"
+client_id = config['client_id']
 
 # Init AWSIoTMQTTClient
 myAWSIoTMQTTClient = None
-if useWebsocket:
-    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
-    myAWSIoTMQTTClient.configureEndpoint(host, 443)
-    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
-else:
-    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
-    myAWSIoTMQTTClient.configureEndpoint(host, 8883)
-    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+myAWSIoTMQTTClient = AWSIoTMQTTClient(client_id)
+myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
 
 # AWSIoTMQTTClient connection configuration
 myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
@@ -71,9 +58,10 @@ myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
 # Connect and subscribe to AWS IoT
 myAWSIoTMQTTClient.connect()
-myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
+myAWSIoTMQTTClient.subscribe('$aws/things/no-lts-ws1/shadow/get/accepted', 1, customCallback)
 
 # Publish to the same topic in a loop forever
+myAWSIoTMQTTClient.publish('$aws/things/no-lts-ws1/shadow/get', None, 1)
 
-while True:
-    time.sleep(1)
+
+time.sleep(5)
